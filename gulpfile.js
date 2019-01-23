f6fedeebd6750bdf0076fbf5b72e4ad8ac06401c
Arjol <arjolbega1996.com>
2019-01-23 18:54:35 +0100

    Completed the installation of the dependencies

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp=require('gulp'),
+sass=require('gulp-sass'),
+watch=require('gulp-watch'),
+browserSync=require('browser-sync').create(),
+wait=require('gulp-wait'),
+autoprefixer=require('gulp-autoprefixer');
+
+/* Styles */
+
+    gulp.task("styles",function(){
+        return gulp.src("./app/assets/styles/styles.scss")
+        .pipe(wait(200))
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .on("error",function(errorInfo){
+            console.log(errorInfo.toString());
+        })
+        .pipe(gulp.dest("./app/css"));
+    });
+    
+    gulp.task('cssInject', ['styles'], function() {
+        return gulp.src('./app/css/*.css')
+          .pipe(browserSync.stream());
+      });
+/*Styles end*/
+
+
+/* Watch */
+
+    gulp.task('watch',function(){
+        browserSync.init({
+           notify:false,
+           server:{
+               baseDir: "./app"
+           } 
+        });
+
+        watch('./app/index.html',function(){
+            browserSync.reload();
+        });
+
+        watch("./app/assets/styles/**/*.scss",function(){
+            gulp.start("cssInject");
+        });
+    });
+
+   
+/* Watch end */
+
+gulp.task('default',['watch']);
